00c3e158491e355c1e0b0ef235515e25dab64ffc
unknown <budidirajeshgoud231@gmail.com>
2024-07-19 23:05:55 +0530

    query to mutation + post

--- src/context/getRestaurantsApi.js
+++ src/context/getRestaurantsApi.js
@@ -2,18 +2,18 @@ import { BASE_URL } from "../Constants";
 import { ApiSlice } from "./ApiSlice";
 
 
-
-
 export const RestaurantApiSlice= ApiSlice.injectEndpoints({
     endpoints:(builder)=>({
         getRestaurants:builder.mutation({
             query:()=>({
-                url: `${BASE_URL}/api/restaurants`
+                url: `${BASE_URL}/api/restaurants`,
+                method: 'POST'
             })
         }),
         getRestaurantById:builder.mutation({
             query:(id)=>({
-                url:`${BASE_URL}/api/restaurants/${id}`
+                url:`${BASE_URL}/api/restaurants/${id}`,
+                method: 'POST'
             })
         })
     })
