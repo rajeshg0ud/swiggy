81fd0a4f70dcf58f4c73cce76b90fd25d354214c
unknown <budidirajeshgoud231@gmail.com>
2024-07-19 23:12:19 +0530

    base url

--- src/Constants.js
+++ src/Constants.js
@@ -1,3 +1,3 @@
-export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : 'https://thetitan-production.up.railway.app';
+export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : 'https://sw-server-y2ol.onrender.com/';
  
 export const USER_URL = '/api/userRouter';
