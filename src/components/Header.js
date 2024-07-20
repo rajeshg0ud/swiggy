f80d22b00103eac35efd466c8354b03189bfca8c
unknown <budidirajeshgoud231@gmail.com>
2024-07-21 02:08:59 +0530

    responsive and slice

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ function Header() {
   return (
     <div className=' z-10 fixed w-full top-0 flex justify-center shadow-md md:py-1 bg-white brightness-105  '>
 
-        <div className='flex w-[98%] md:w-[80%]  justify-between items-center  text-slate-700 '>
+        <div className='flex w-[97%] md:w-[80%]  justify-between items-center  text-slate-700 '>
         
         <div className='flex items-center'>
           <div className=' w-6 md:w-[36px]'>
@@ -20,7 +20,7 @@ function Header() {
           </div>
 
           <div className='flex items-center hover:cursor-pointer m-3 md:m-7 my-6 text-sm '>
-           <p className=' hover:text-orange-500 border-gray-800 border-b-2 hover:border-orange-500  text-sm md:text-md pb-1 mx-2 font-bold'>Banjara Hills </p> 
+           <p className=' hover:text-orange-500 border-gray-800 border-b-2 hover:border-orange-500  text-sm md:text-md pb-1 mx-[5px] md:mx-2 font-bold'>Banjara Hills </p> 
            <p className='pr-1 pb-1 text-gray-600 hidden md:block'> Hyderabad, Telangana, India </p>
            <FontAwesomeIcon icon={faAngleDown} className=' text-orange-500'/>
           </div>
