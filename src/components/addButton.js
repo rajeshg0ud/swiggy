399af6990d2a2b54a24c46977c3f0f8957296095
unknown <budidirajeshgoud231@gmail.com>
2024-07-21 09:12:00 +0530

    responsive and slice

--- src/components/addButton.js
+++ src/components/addButton.js
@@ -7,7 +7,7 @@ function AddButton({ resInfo, value, res }) {
   const dispatch = useDispatch();
   return (
     <button
-      className={`bg-white px-5 md:px-7 py-1 mr-6 border-[1.57px] rounded-sm text-green-500 border-green-500 text-sm font-semibold hover:shadow-lg hover:bg-green-50 ${res?.card?.info?.imageId ? 'absolute top-20 md:top-24 left-5 md:left-7' : 'h-8'}`}
+      className={`bg-white px-5 md:px-7 py-1 border-[1.57px] rounded-sm text-green-500 border-green-500 text-sm font-semibold hover:shadow-lg hover:bg-green-50 ${res?.card?.info?.imageId ? 'absolute top-20 mr-6 md:top-24 left-5 md:left-7' : ' mr-[18.4px] md:mr-6 h-8'}`}
       onClick={() => dispatch(addToCart({
         id: resInfo.id,
         resName: resInfo.name,
